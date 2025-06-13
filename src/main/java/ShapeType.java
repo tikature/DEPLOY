20add10bb823f4713a066b52c63a25bb8f54281c
tikature <detikaa10@gmail.com>
2025-06-13 20:13:29 +0700

    up

--- src/main/java/ShapeType.java
+++ src/main/java/ShapeType.java
@@ -1,21 +1,7 @@
-import src.main.java.Counter;
+import src.main.java;
 
-public class Driver {
-
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.increment();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-	}
-
+public enum ShapeType {
+	CIRCLE,
+	SQUARE,
+	RECTANGLE
 }
